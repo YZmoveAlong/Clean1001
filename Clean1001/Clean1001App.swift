b45747a0dbf9288cbb497c9e2a584cd4cbc69806
Slamet <slamet.riyadi@noble-software.com>
2024-07-02 13:38:32 +0700

    Initial Commit

---
+++ Clean1001/Clean1001App.swift
@@ -0,0 +1,17 @@
+//
+//  Clean1001App.swift
+//  Clean1001
+//
+//  Created by Slamet on 02/07/24.
+//
+
+import SwiftUI
+
+@main
+struct Clean1001App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Clean1001/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Clean1001
+//
+//  Created by Slamet on 02/07/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
